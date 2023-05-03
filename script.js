d5d74061ffddb9ee418c3695018657e7019d194c
Brian Werner <brianmw199@gmail.com>
2023-05-02 23:54:58 -0500

    created three different sets of conditionals with the first set the first number being bigger the second set the second number being bigger and the third being the same

--- script.js
+++ script.js
@@ -20,3 +20,41 @@ function addNumbers() {
     console.log('The total is',sum = num1 + num2);
 }
 addNumbers();
+
+// Conditionals with different sets of numbers, 1st set is the 1st number was bigger
+// the 2nd set the 2nd number was bigger and the 3rd set both numbers are the same
+
+// created a function that takes 2 numbers, with the 1st number being greater than the second
+
+let num1 = 8;
+let num2 = 3;
+function firstBiggerNumber() {
+}
+if (num1 > num2) {
+  console.log(firstBiggerNumber = num1);
+}
+console.log('First number was bigger!');
+
+// Created a function that takes 2 numbers, with the second number being greater than first number
+
+let num3 = 6;
+let num4 = 10;
+function secondBiggerNumber() {
+}
+if (num3 < num4) {
+  console.log(secondBiggerNumber = num4);
+}
+console.log('Second number was bigger!');
+
+// created a function that takes 2 numbers that are the same
+
+
+let num5 = 7;
+let num6 = 7;
+function sameNumber() {
+  if (num5 === num6) {
+    console.log(sameNumber);
+  }
+  
+}
+console.log('The numbers are the same!', num5 = num6);
