95b8acf194c6aada8a8b147e91e1f9c502bf0e28
Brian Werner <brianmw199@gmail.com>
2023-05-02 22:27:25 -0500

    Did a set of different addition math problems there are 2 problems using a return key and one that is not

---
+++ script.js
@@ -0,0 +1,22 @@
+// Additon problems for micro challenge
+// added 2 numbers together with a return
+function addTwoNumbers(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+
+}
+console.log( addTwoNumbers(4, 3));
+
+// added 3 numbers together with a return
+function addThreeNumbers(firstNumber, secondNumber, thirdNumber) {
+  return firstNumber + secondNumber + thirdNumber;
+}
+console.log(addThreeNumbers(5, 8, 3));
+
+// added 2 numbers toghether without using a return
+function addNumbers() {
+    let num1 = 2;
+    let num2 = 3;
+    let sum = num1 + num2;
+    console.log('The total is',sum = num1 + num2);
+}
+addNumbers();
